bcba12a80c435497701f1fa262fb820d5425ce8a
Emily <em.chin@live.com>
2023-11-06 15:31:58 -0800

    removed setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-'''
-EC Note: updated versions of pip do NOT require a setup.py file but older versions will, so I'm including just in case :)
-'''
-from setuptools import setup
-
-if __name__ == '__main__':
-    setup()
